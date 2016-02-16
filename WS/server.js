7fbd169f6f741d66cb5fbae7e98a2806a245fe30
Yohel <yoe32ns@hotmail.com>
2016-02-16 12:51:31 -0600

    Starting

---
+++ WS/server.js
@@ -0,0 +1,55 @@
+var express = require("express");
+var mysql   = require("mysql");
+var bodyParser  = require("body-parser");
+var md5 = require('MD5');
+var rest = require("./rest.js");
+var app  = express();
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+function REST(){
+    var self = this;
+    self.connectMysql();
+};
+
+REST.prototype.connectMysql = function() {
+    var self = this;
+    var pool      =    mysql.createPool({
+        connectionLimit : 100,
+        host     : process.env.OPENSHIFT_MYSQL_DB_HOST,
+        user     : 'admin7V7YxvE',
+        password : 'bIaNIfvdSskC',
+        database : 'restful_api_demo',
+        debug    :  false
+    });
+    pool.getConnection(function(err,connection){
+        if(err) {
+          self.stop(err);
+        } else {
+          self.configureExpress(connection);
+        }
+    });
+}
+
+REST.prototype.configureExpress = function(connection) {
+      var self = this;
+      app.use(bodyParser.urlencoded({ extended: true }));
+      app.use(bodyParser.json());
+      var router = express.Router();
+      app.use('/api', router);
+      var rest_router = new rest(router,connection,md5);
+      self.startServer();
+}
+
+REST.prototype.startServer = function() {
+      app.listen(server_port,server_ip_address,function(){
+          console.log("All right ! I am alive at Port: " + server_ip_address +":"+  server_port);
+      });
+}
+
+REST.prototype.stop = function(err) {
+    console.log("ISSUE WITH MYSQL n" + err);
+    process.exit(1);
+}
+
+new REST();
