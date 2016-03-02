da258f5c60f4fe8b2b5431e6814ce8e1574628ff
Sharel <ge_shareliz@hotmail.es>
2016-03-02 15:54:48 -0600

    'ConRouteActivityFuncionando'

---
+++ Android/Bus/app/src/main/java/com/dev/p3/bus/NavDrawerItem.java
@@ -0,0 +1,19 @@
+package com.dev.p3.bus;
+
+
+public class NavDrawerItem {
+
+    private String title;
+
+    public NavDrawerItem(){}
+
+    public NavDrawerItem(String title){
+        this.title = title;
+    }
+
+
+    public String getTitle(){
+        return this.title;
+    }
+
+}
